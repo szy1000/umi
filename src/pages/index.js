2779ff86bf082fea71d364c9b40b1e2c949fe874
szy1000 <1538078053@qq.com>
2019-05-23 23:30:57 +0800

    first page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,22 @@
+import {Card,Layout} from 'antd'
+
+const { Header, Footer, Sider, Content } = Layout;
+
 export default () => {
-    return <div>hello world</div>
+  const style = {
+    width: '400px',
+    margin: '30px',
+    boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)',
+    border: '1px solid #e8e8e8',
+  };
+  return <Layout>
+      <Sider>Sider</Sider>
+      <Layout>
+        <Header>Header <div>hello world</div></Header>
+        <Content>Content
+          <Card style={style} actions={[<a>操作一</a>, <a>操作二</a>]}>
+        </Card></Content>
+        <Footer>Footer</Footer>
+      </Layout>
+    </Layout>
 }
