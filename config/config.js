a7f8f9f482d343497b6f3922e7e57807e57e52d1
szy1000 <1538078053@qq.com>
2019-05-24 10:31:55 +0800

    add router page

--- config/config.js
+++ config/config.js
@@ -2,11 +2,35 @@ export default {
   plugins: [
     ['umi-plugin-react', {
       antd: true,
+      extends: "eslint-config-umi",
       // 这里暂时还没有添加配置，该插件还不会有作用，我们会在后面的课程按照需求打开相应的配置
     }],
   ],
   routes: [{
     path: '/',
-    component: './index',
+    component: '../layout/',
+    routes: [
+      {
+        path: '/',
+        redirect: '/index',
+
+      },
+      {
+        path: '/index',
+        component: 'index',
+      },
+      {
+        path: 'about',
+        component: './about/'
+      },
+      {
+        path: '/dashboard',
+        routes: [
+          { path: './analysis', component: 'dashboard/analysis' },
+          { path: './monitor', component: 'dashboard/monitor' },
+          { path: './workplace', component: 'dashboard/workplace' }
+        ]
+      }
+    ]
   }]
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,5 @@
-import {Card,Layout} from 'antd'
-
-const { Header, Footer, Sider, Content } = Layout;
+import {Card} from 'antd'
 
 export default () => {
-  const style = {
-    width: '400px',
-    margin: '30px',
-    boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)',
-    border: '1px solid #e8e8e8',
-  };
-  return <Layout>
-      <Sider>Sider</Sider>
-      <Layout>
-        <Header>Header <div>hello world</div></Header>
-        <Content>Content
-          <Card style={style} actions={[<a>操作一</a>, <a>操作二</a>]}>
-        </Card></Content>
-        <Footer>Footer</Footer>
-      </Layout>
-    </Layout>
+  return <Card actions={[<a>操作一</a>, <a>操作二</a>]}></Card>
 }
