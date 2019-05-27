beb73237a1008d82cb70dd17d7f66c35d71bbad3
szy1000 <1538078053@qq.com>
2019-05-27 21:49:52 +0800

    fixed some

--- src/layout/index.js
+++ src/layout/index.js
@@ -35,17 +35,13 @@ class BasicLayout extends Component {
             </Link>
           </div>
           <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-            <Menu.Item key="1">
-              <Icon type="pie-chart"/>
-              <span>Helloworld</span>
-            </Menu.Item>
             <SubMenu
               key="sub1"
               title={<span><Icon type="dashboard"/><span>Dashboard</span></span>}
             >
-              <Menu.Item key="2"><Link to="/dashboard/analysis">分析页</Link></Menu.Item>
-              <Menu.Item key="3"><Link to="/dashboard/monitor">监控页</Link></Menu.Item>
-              <Menu.Item key="4"><Link to="/dashboard/workplace">工作台</Link></Menu.Item>
+              <Menu.Item key="1"><Link to="/dashboard/analysis">分析页</Link></Menu.Item>
+              <Menu.Item key="2"><Link to="/dashboard/monitor">监控页</Link></Menu.Item>
+              <Menu.Item key="3"><Link to="/dashboard/workplace">工作台</Link></Menu.Item>
             </SubMenu>
           </Menu>
         </Sider>
